6d461ce8170d8536f415642129e36d0c282f4dfa
Dave Trabka <dtrabka@gmail.com>
2018-08-21 12:45:47 -0400

    fix(summary): fixed typos

--- client/components/summary.js
+++ client/components/summary.js
@@ -6,22 +6,23 @@ const Summary = () => (
     <div className="booking-container">
       <br />
       <h6 className="teal-text">
-        Just steps from the Katy Trail, Missouri River, Entertainment and Depot
-        District, casino and downtown Boonville! Enjoy the beauty and peace of
-        this historic town with the convenience of Columbia, MO only 18 miles
-        away!
+        Our home is just steps from the Katy Trail, Missouri River,
+        Entertainment and Depot District, casino, and downtown Boonville! Enjoy
+        the beauty and peace of this historic town with the convenience of
+        Columbia, MO only 18 miles away!
       </h6>
       <h6 className="teal-text">
         Visit the world-famous Anheuser-Busch Clydesdales at the Warm Springs
-        Ranch, bike or hike the Katy Trail, visit a local wineryl or spend a day
+        Ranch, bike or hike the Katy Trail, visit a local winery or spend a day
         or two exploring the rich history of the area - it's a getaway that
         won't break the bank!
       </h6>
       <br />
+      <br />
       <Link
         to="/neighborhood"
         className="btn-large waves-effect waves-light teal lighten-1">
-        Explore our favorite Boonville activities!
+        Explore our favorite Boonville activities
       </Link>
       <br />
     </div>
